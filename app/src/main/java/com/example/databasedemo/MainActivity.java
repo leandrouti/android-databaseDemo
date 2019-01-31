5ea11f6fabb1f90cd229d62ed504f9e74658f115
Utiyama Tatsuya Leandro <leandrouti@yahoo.com.br>
2019-01-31 15:36:36 +0900

    Added SQLite database

--- app/src/main/java/com/example/databasedemo/MainActivity.java
+++ app/src/main/java/com/example/databasedemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.databasedemo;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        try{
+            SQLiteDatabase myDatabase = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);
 
+            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR, age INT(3))");
+
+            myDatabase.execSQL("INSERT INTO users(name, age) VALUES('Rob', 34)");
+            myDatabase.execSQL("INSERT INTO users(name, age) VALUES('Carlos', 10)");
+
+            Cursor c = myDatabase.rawQuery("SELECT * FROM users", null);
+
+            int nameIndex = c.getColumnIndex("name");
+            int ageindex = c.getColumnIndex("age");
+
+            c.moveToFirst();
+
+            while(c != null) {
+                Log.i("name ", c.getString(nameIndex));
+                Log.i("age ", Integer.toString(c.getInt(ageindex)));
+                c.moveToNext();
+            }
+
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
